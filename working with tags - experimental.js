3573a5d9d970218eaa2581765b8abf22fec6f151
Jonathan A. Whiteley <jawhiteley@me.com>
2015-12-23 00:36:58 -0500

    example script working with tags
    batch-edit tags using regex matching and substitution.
    Affects *all* records with matching tags.
    
    Signed-off-by: Jonathan A. Whiteley <jawhiteley@me.com>

---
+++ working with tags - experimental.js
@@ -0,0 +1,46 @@
+/* In Zotero, there are 2 ways to edit tags, and thus two processes to 'batch'
+ * 1. Rename Tag: this applies to ALL entries with that tag, selected or not.
+ * 2. Edit Tag under "Tags" tab in edit pane (right column): this applies only to that entry, and can result in a tag being created.
+ */
+
+// regex cannot be enclosed in quotes.
+var oldpattern = /^\>\s*/
+var oldval = '>'
+var newval = '# '
+
+var ZoteroPane = Components.classes["@mozilla.org/appshell/window-mediator;1"] 
+        .getService(Components.interfaces.nsIWindowMediator)
+        .getMostRecentWindow("navigator:browser").ZoteroPane;
+
+var selected_items = ZoteroPane.getSelectedItems();
+
+Zotero.Tags.match = function (pattern) {
+  // regex matching on tag names
+  var all_tags = Zotero.Tags.search();
+  var found_tags = [];
+  for (var id in all_tags) {
+    tag_name = all_tags[id].name;
+    if ( tag_name.match(oldpattern) ) {
+      found_tags.push(tag_name);
+    }
+  }
+  return found_tags;    // should probably return IDs or something; compare with Zotero.Tags.search()
+  // https://github.com/zotero/zotero/blob/4dd56eadfca9b89e1728661776407337bb823ebd/chrome/content/zotero/xpcom/data/tags.js#L266
+}
+
+var found_tags = Zotero.Tags.match(oldpattern);
+// alert(found_tags);
+
+for (var tag of found_tags) {  // iterate over array: experimental. Use numeric index for compatibility with older browsers.
+  // alert(tag);
+  tagType = 0;  // custom tags: tags generated automatically by Zotero are type '1'
+  tagID = Zotero.Tags.getID(tag, tagType);  // libraryID is optional; defaults to the current window, I think.
+  // alert(Zotero.Tags.getName(tagID));
+  tagIDs = Zotero.Tags.getIDs(tag);  // libraryID is optional?
+  // alert(Zotero.Tags.getName(tagIDs));
+
+  var new_name = tag.replace(oldpattern, newval);
+  alert("      " + tag + "\n=>  " + new_name);
+  // Zotero.Tags.rename(tagIDs, new_name);  // This will rename the tag for ALL entries.
+}
+
